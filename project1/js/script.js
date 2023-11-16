3ee451751a9430e87acb50069a8d436f44c57390
Goran <148502119+goran12345678@users.noreply.github.com>
2023-11-16 05:43:10 +0000

    Update script.js

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,1 +1,43 @@
+function clientFormSubmit(e) {
+    let form = document.forms[0]
+    let fname = form["fname"].value
+    let lname = form["lname"].value
+    let email = form["email"].value
+    console.log("fname :", fname)
+    console.log("lname: ", lname)
+    console.log("email: ", email)
 
+    if (fname == "")
+        alert('enter first name')
+    else if (lname == "")
+        alert('enter last name')
+    else if (email == "")
+        alert('enter email')
+
+    alert("pause")
+}
+function scheculeSubmissionSubmit() {
+    let arr = ["item 1","item 2","item 3"] //zero basea array
+    //arr[0]
+    let form = document.forms[0]
+    let client_id = form["client_id"].value
+    let agenda = form["agenda"].value
+    let place = form["place"].value
+    let date = form["date"].value
+    let time = form["time"].value
+
+    if (client_id == "")
+        alert("enter client id")
+    else if (date == "")
+        alert("enter a date")
+    else if (date != "") {
+        let current = Date.now()
+        if (date < current)
+            alert("pick a date in the future")
+    }
+    else if (time == "")
+        alert("enter a time")
+
+
+    alert("pause")
+}
