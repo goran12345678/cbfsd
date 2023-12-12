8f33f90ff7138dc99d93806e5d03cf60fb0d0132
Goran <goran.djuric@simplilearn.net>
2023-12-12 05:03:11 +0000

    session end

---
+++ Course2/Example/src/Example.java
@@ -0,0 +1,70 @@
+
+public class Example {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		
+		//Unary Operators -- (decrement operator),++ (increment operator), ! (negation operator), ~ (bitwise inverse operator)
+		//int x = 10;
+		//System.out.println(x);
+		//System.out.println(++x);
+		//System.out.println(x);
+		
+		//System.out.println(x--);
+		//System.out.println(x);
+		
+		//boolean y = false;
+		//System.out.println(y);
+		//System.out.println(!y);
+		
+		//int a = 10;  // 0000 0110  2^3 + 2^1 = 1-
+		//System.out.println(~a); // 1111 1001
+		
+		//0000 1110 = 2^3 + 2^2 + 2^1 = 12
+		
+		//Arithmetic Operator
+		
+		/*int a = 10;
+		int b = 5;
+		System.out.println(a+b); //addition operator
+		System.out.println(a-b);// subtraction operator
+		System.out.println(a*b); //multiplication operator
+		System.out.println(a/b); //division operator
+		System.out.println(a%b);//modulus operator (the remainder of the division)
+		
+		System.out.println(9*9/4+2-1*4/2);
+		*/
+		
+		//Left Shift Operator <<
+		//System.out.println(8<<2); // 8*2^2 = 32
+		//Right Shift Operator >>
+		//System.out.println(8>>2); // 8/2^2 = 2
+		//Java Shift Operator 
+		//System.out.println(20>>2); //0001 0010 0000 1001
+		
+		//32<<4 32/4^2
+		
+		//AND Operator &&
+		int a = 9;
+		int b = 4;
+		int c = 19;
+		System.out.println(a>b && a>c);
+		
+		//OR Operator ||
+		System.out.println(a<b || a>c || 3>1);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
