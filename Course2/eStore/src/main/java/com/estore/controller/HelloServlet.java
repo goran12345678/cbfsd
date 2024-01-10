c2ff756481c24f67818020e0d1f3a0c104af9e50
Goran <goran.djuric@simplilearn.net>
2024-01-10 00:23:48 +0000

    session end

---
+++ Course2/eStore/src/main/java/com/estore/controller/HelloServlet.java
@@ -0,0 +1,53 @@
+package com.estore.controller;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String msg;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        System.out.println("Helloservlet - constructor executed");
+        msg = "Thank you for shopping";
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException{
+    	super.init(config);
+    	System.out.println("HelloServlet - init executed");
+    }
+    
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("HelloServlet - service executed");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<h3> " + msg + "</h3>");
+		
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("HelloServlet - destroy executed");
+	}
+}
---
+++ Course2/eStore/src/main/java/com/estore/controller/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.estore.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		System.out.print("User login: " + email + " " + password);
+		
+		String html = "<center><h3>Login was OK!!</h3></center>";
+		response.setContentType("text/html"); //HTTP header added
+		PrintWriter out = response.getWriter(); // the stream writer
+		
+		out.print(html); //send message body to the browser. the message is html
+		
+	}
+
+}
