c463c56ef3a45844acdce7c7e58963003a34f971
Goran <goran.djuric@simplilearn.net>
2024-02-27 05:45:03 +0000

    session end

---
+++ Course3/springbootinitializerdemo/src/main/java/com/fullstack/springbootinitializerdemo/SpringbootinitializerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.springbootinitializerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinitializerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootinitializerdemoApplication.class, args);
+	}
+
+}
---
+++ Course3/springbootinitializerdemo/src/test/java/com/fullstack/springbootinitializerdemo/SpringbootinitializerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.springbootinitializerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootinitializerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
